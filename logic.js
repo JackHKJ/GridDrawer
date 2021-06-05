7783cf4a27babbe4c9c653e95c235b1cf27fe149
JackHKJ <60669490+JackHKJ@users.noreply.github.com>
2021-06-06 01:39:57 +0800

    Easy-to-use update
    Text input will be automatically focused and selected, modifications will be automatically saved when clicking another box. Context click on a cell will clear all the borders and bg color, scroll the wheel up will modify the bg color and down the border color.

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
+// This variable records the current selected parameter
 var selected_element = null
+// This are the available color list for the designed elements
 var border_color = {
     white: "white",
     red: "red",
@@ -16,10 +18,10 @@ var bg_color = {
     black: "black"
 }
 
-
+// This is the default function for creating the grid
 function create_grid(){
     // Create a grid of the given size
-    console.log("Creating grid")
+    console.log("Creating a new grid")
     // Get the size
     var size = parseInt(document.getElementById("grid_size_input").value)   
     var grid = document.getElementById("grid_section")
@@ -32,7 +34,13 @@ function create_grid(){
 
             var this_element = document.createElement("div")
             this_element.setAttribute("name",i+'-'+j)
-            this_element.setAttribute('onclick', 'select(this)')
+            this_element.setAttribute('onclick', 'select(this)') 
+            // this_element.setAttribute('oncontextmenu', 'clear(this)')
+            this_element.addEventListener('contextmenu', clear) 
+            // this_element.setAttribute('onwheel','scroll_handler(this)')
+            this_element.addEventListener('wheel',scroll_handler)
+
+
             this_element.setAttribute('border_color', border_color.white)
             this_element.setAttribute('background_color', bg_color.white)
 
@@ -64,32 +72,35 @@ function create_grid(){
     }
 }
 
-
+// This function will be called when a grid-cell is clicked (Select)
 function select(e){
+    modify_selected()
     selected_element = e
     document.getElementById("selected_name").textContent = e.getAttribute("name")   
 
     sel = document.getElementById("selected_border_color")
     var options = Array.from(sel.options);
-    console.log(options)
+    // console.log(options)
     options.forEach((option, i) => {
-        console.log(option,i)
+        // console.log(option,i)
       if (option.value === selected_element.getAttribute("border_color")) sel.selectedIndex = i;
     });
 
     sel = document.getElementById("selected_bg_color")
     var options = Array.from(sel.options);
-    console.log(options)
+    // console.log(options)
     options.forEach((option, i) => {
-        console.log(option,i)
+        // console.log(option,i)
       if (option.value === selected_element.getAttribute("background_color")) sel.selectedIndex = i;
     });
 
-
-
-    document.getElementById("selected_text").value = e.innerText
+    let selected_text = document.getElementById("selected_text")
+    selected_text.value = e.innerText
+    selected_text.focus(function() {this.select()} )
+    
 }
 
+// This function is used to modify the display of the current cell
 function modify_selected(e){
     if(selected_element == null){
         return
@@ -105,3 +116,66 @@ function modify_selected(e){
     selected_element.style.background = color_value 
 
 }
+
+// This function is used to modify the selected cell, which will clear all the border/bg_color on it
+function clear(e){
+    e.preventDefault()
+    // console.log("rk")
+    // console.log(e)
+    var target_element = e.target
+    // console.log(target_element)
+    select(target_element)
+    document.getElementById("selected_border_color").value = border_color.white
+    document.getElementById("selected_bg_color").value = bg_color.white
+    modify_selected(target_element)
+}
+
+// This function is to handle the mouse scroll
+function scroll_handler(e){
+    e.preventDefault()
+    // console.log("scrolled")
+    // console.log(e)
+    var target_element = e.target
+    var delta = parseFloat(e.deltaY)
+    // console.log(delta)    
+    select(target_element)
+    // console.log(selected_element.getAttribute("background_color"))
+
+    if(delta < 0){
+        // DeltaY < 0: Scroll on top
+        let flag = false
+        document.getElementById("selected_bg_color").value = Object.entries(bg_color)[0][0]
+        for(const [key, value] of Object.entries(bg_color)){
+
+            if(!flag && value == selected_element.getAttribute("background_color")){
+                flag = true
+                continue
+            }          
+            if(flag){
+                document.getElementById("selected_bg_color").value = value
+                flag = false
+                break
+            }
+        }
+    }
+    else{
+        // DeltaY > 0: Scroll to bottom
+        let flag = false
+        document.getElementById("selected_border_color").value = Object.entries(bg_color)[0][0]
+        for(const [key, value] of Object.entries(border_color)){
+
+            if(!flag && value == selected_element.getAttribute("border_color")){
+                flag = true
+                continue
+            }          
+            if(flag){
+                document.getElementById("selected_border_color").value = value
+                flag = false
+                break
+            }
+        }
+    }
+
+    modify_selected(target_element)
+}
+
