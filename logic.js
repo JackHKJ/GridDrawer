e6c10ced554f1dba3ad3055efee30272fdc74f2d
JackHKJ <60669490+JackHKJ@users.noreply.github.com>
2021-06-10 15:01:18 +0800

    Added method to thicken the border

--- logic.js
+++ logic.js
@@ -1,5 +1,13 @@
 // This variable records the current selected parameter
-var selected_element = null
+var SELECTED_ELEMENT = null
+// The mode indicator, used to change current mode
+var MODE_DICT = {
+    content:"content",
+    border:"border"
+}
+var CURRENT_MODE = MODE_DICT.content
+// This is used for the thick_border value iteration
+var THICK_LIST = ['00','10','11','01']
 // This are the available color list for the designed elements
 var COLOR_LIBRARY = {
     white: "white",
@@ -35,8 +43,9 @@ function create_grid(){
             this_element.addEventListener('wheel',scroll_handler)
 
 
-            this_element.setAttribute('COLOR_LIBRARY', COLOR_LIBRARY.white)
+            this_element.setAttribute('border_color', COLOR_LIBRARY.white)
             this_element.setAttribute('background_color', COLOR_LIBRARY.white)
+            this_element.setAttribute('thick_border','0000') //[top, right][bottom left]
 
             if(i==0||i==size+1||j==0||j==size+1){
                 this_element.className="clear_cell"
@@ -69,7 +78,7 @@ function create_grid(){
 // This function will be called when a grid-cell is clicked (Select)
 function select(e){
     modify_selected()
-    selected_element = e
+    SELECTED_ELEMENT = e
     document.getElementById("selected_name").textContent = e.getAttribute("name")   
 
     sel = document.getElementById("selected_border_color")
@@ -77,7 +86,7 @@ function select(e){
     // console.log(options)
     options.forEach((option, i) => {
         // console.log(option,i)
-      if (option.value === selected_element.getAttribute("COLOR_LIBRARY")) sel.selectedIndex = i;
+      if (option.value === SELECTED_ELEMENT.getAttribute("border_color")) sel.selectedIndex = i;
     });
 
     sel = document.getElementById("selected_bg_color")
@@ -85,7 +94,7 @@ function select(e){
     // console.log(options)
     options.forEach((option, i) => {
         // console.log(option,i)
-      if (option.value === selected_element.getAttribute("background_color")) sel.selectedIndex = i;
+      if (option.value === SELECTED_ELEMENT.getAttribute("background_color")) sel.selectedIndex = i;
     });
 
     let selected_text = document.getElementById("selected_text")
@@ -96,25 +105,25 @@ function select(e){
 
 // This function is used to modify the display of the current cell
 function modify_selected(e){
-    if(selected_element == null){
+    if(SELECTED_ELEMENT == null){
         return
     }
     // Update the text value 
-    selected_element.innerText = document.getElementById("selected_text").value
+    SELECTED_ELEMENT.innerText = document.getElementById("selected_text").value
     // Update the border color
-    selected_element.setAttribute("COLOR_LIBRARY", document.getElementById("selected_border_color").value)
-    color_value = document.getElementById("selected_border_color").value + " solid 4px"    
-    selected_element.style.border = color_value 
+    SELECTED_ELEMENT.setAttribute("border_color", document.getElementById("selected_border_color").value)
+    color_value = document.getElementById("selected_border_color").value + " inset 0 0 0 4px"    
+    SELECTED_ELEMENT.style.boxShadow = color_value 
     // Update the background color
-    selected_element.setAttribute("background_color", document.getElementById("selected_bg_color").value)
+    SELECTED_ELEMENT.setAttribute("background_color", document.getElementById("selected_bg_color").value)
     color_value = document.getElementById("selected_bg_color").value
-    selected_element.style.background = color_value 
+    SELECTED_ELEMENT.style.background = color_value 
     // Adjust the font color if the color value is black
     if(DARK_COLORS.includes(color_value)){
-        selected_element.style.color = COLOR_LIBRARY.white
+        SELECTED_ELEMENT.style.color = COLOR_LIBRARY.white
     }
     else{
-        selected_element.style.color = COLOR_LIBRARY.black
+        SELECTED_ELEMENT.style.color = COLOR_LIBRARY.black
     }
 }
 
@@ -140,42 +149,106 @@ function scroll_handler(e){
     var delta = parseFloat(e.deltaY)
     // console.log(delta)    
     select(target_element)
-    // console.log(selected_element.getAttribute("background_color"))
-
-    if(delta < 0){
-        // DeltaY < 0: Scroll on top
-        let flag = false
-        document.getElementById("selected_bg_color").value = Object.entries(COLOR_LIBRARY)[0][0]
-        for(const [key, value] of Object.entries(COLOR_LIBRARY)){
-
-            if(!flag && value == selected_element.getAttribute("background_color")){
-                flag = true
-                continue
-            }          
-            if(flag){
-                document.getElementById("selected_bg_color").value = value
-                flag = false
-                break
+    // console.log(SELECTED_ELEMENT.getAttribute("background_color"))
+    // Under content mode:
+    if (CURRENT_MODE == MODE_DICT.content){
+        if(delta < 0){
+            // DeltaY < 0: Scroll on top
+            let flag = false
+            document.getElementById("selected_bg_color").value = Object.entries(COLOR_LIBRARY)[0][0]
+            for(const [key, value] of Object.entries(COLOR_LIBRARY)){
+    
+                if(!flag && value == SELECTED_ELEMENT.getAttribute("background_color")){
+                    flag = true
+                    continue
+                }          
+                if(flag){
+                    document.getElementById("selected_bg_color").value = value
+                    flag = false
+                    break
+                }
+            }
+        }
+        else{
+            // DeltaY > 0: Scroll to bottom
+            let flag = false
+            document.getElementById("selected_border_color").value = Object.entries(COLOR_LIBRARY)[0][0]
+            for(const [key, value] of Object.entries(COLOR_LIBRARY)){
+    
+                if(!flag && value == SELECTED_ELEMENT.getAttribute("border_color")){
+                    flag = true
+                    continue
+                }          
+                if(flag){
+                    document.getElementById("selected_border_color").value = value
+                    flag = false
+                    break
+                }
             }
         }
     }
+    // Under border mode, modify the thick_border iteratively
     else{
-        // DeltaY > 0: Scroll to bottom
-        let flag = false
-        document.getElementById("selected_border_color").value = Object.entries(COLOR_LIBRARY)[0][0]
-        for(const [key, value] of Object.entries(COLOR_LIBRARY)){
-
-            if(!flag && value == selected_element.getAttribute("COLOR_LIBRARY")){
-                flag = true
-                continue
-            }          
-            if(flag){
-                document.getElementById("selected_border_color").value = value
-                flag = false
-                break
-            }
+        let current_thick_status = target_element.getAttribute('thick_border')
+        TR = current_thick_status[0]+current_thick_status[1]
+        BL = current_thick_status[2]+current_thick_status[3]
+        console.log(TR)
+        console.log(BL)
+        // Modify [top right]
+        if(delta < 0){
+          let index = THICK_LIST.indexOf(TR)
+          index = (index+1)%4
+          TR = THICK_LIST[index]
         }
+        // Modify [bottom left]
+        else{
+            let index = THICK_LIST.indexOf(BL)
+            index = (index+1)%4
+            BL = THICK_LIST[index]
+        }
+        current_thick_status = TR+BL
+        console.log(current_thick_status)
+        thick_border_modifier(target_element, current_thick_status)
+        target_element.setAttribute('thick_border', current_thick_status)
     }
+    
     modify_selected(target_element)
 }
 
+function change_mode(e){
+    if(CURRENT_MODE == MODE_DICT.content){
+        CURRENT_MODE = MODE_DICT.border
+        document.getElementById("mode_name").textContent = MODE_DICT.border
+    }
+    else{
+        CURRENT_MODE = MODE_DICT.content
+        document.getElementById("mode_name").textContent = MODE_DICT.content
+    }
+}
+
+function thick_border_modifier(target, status){
+    if(status[0] === "1"){
+        target.style.borderTop = "2px black solid"
+    }
+    else{
+        target.style.borderTop = '2px rgb(200, 200, 200) solid'
+    }
+    if(status[1] === "1"){
+        target.style.borderRight = "2px black solid"
+    }
+    else{
+        target.style.borderRight = '2px rgb(200, 200, 200) solid'
+    }
+    if(status[2] === "1"){
+        target.style.borderBottom = "2px black solid"
+    }
+    else{
+        target.style.borderBottom = '2px rgb(200, 200, 200) solid'
+    }
+    if(status[3] === "1"){
+        target.style.borderLeft = "2px black solid"
+    }
+    else{
+        target.style.borderLeft = '2px rgb(200, 200, 200) solid'
+    }
+}
