8d3133eab0d1900ffd9ee8aabcda500a4ba33ebe
JackHKJ <60669490+JackHKJ@users.noreply.github.com>
2021-06-12 12:21:59 +0800

    Enable using icon as background
    Now custom image is supported. Use the upload panel on the bottom to upload the icon, then change mode to 'image', scroll mouse to top will change the background image where down will still change the border.

--- logic.js
+++ logic.js
@@ -3,7 +3,8 @@ var SELECTED_ELEMENT = null
 // The mode indicator, used to change current mode
 var MODE_DICT = {
     content:"content",
-    border:"border"
+    border:"border",
+    image:"image"
 }
 var CURRENT_MODE = MODE_DICT.content
 // This is used for the thick_border value iteration
@@ -17,17 +18,21 @@ var COLOR_LIBRARY = {
     blue: "royalblue",
     black: "black"
 }
-
+// These are the colors that needs white font color
 var DARK_COLORS = ["black","royalblue","red"]
+// This is the default list for storing the src for the icons
+var ICON_SRC_LIST = ['url("$NONE$")']
+
+
 
 // This is the default function for creating the grid
 function create_grid(){
     // Create a grid of the given size
-    console.log("Creating a new grid")
+    // console.log("Creating a new grid")
+    CURRENT_MODE = MODE_DICT.content
     // Get the size
     var size = parseInt(document.getElementById("grid_size_input").value)   
     var size_sup = parseInt(document.getElementById("grid_size_input_support").value)   
-    console.log(size_sup)
     if (size_sup == parseInt("NaN")){
         size_sup = size
     }
@@ -46,11 +51,13 @@ function create_grid(){
             this_element.addEventListener('contextmenu', clear) 
             // this_element.setAttribute('onwheel','scroll_handler(this)')
             this_element.addEventListener('wheel',scroll_handler)
-
+            
 
             this_element.setAttribute('border_color', COLOR_LIBRARY.white)
             this_element.setAttribute('background_color', COLOR_LIBRARY.white)
             this_element.setAttribute('thick_border','0000') //[top, right][bottom left]
+            // this_element.style.backgroundImage = ICON_SRC_LIST[0]
+            this_element.setAttribute('background_index',0)
 
             if(i==0||i==size+1||j==0||j==size_sup+1){
                 this_element.className="clear_cell"
@@ -82,7 +89,7 @@ function create_grid(){
 
 // This function will be called when a grid-cell is clicked (Select)
 function select(e){
-    modify_selected()
+    // modify_selected()
     SELECTED_ELEMENT = e
     document.getElementById("selected_name").textContent = e.getAttribute("name")   
 
@@ -106,6 +113,7 @@ function select(e){
     selected_text.value = e.innerText
     selected_text.focus(function() {this.select()} )
     
+    modify_selected()
 }
 
 // This function is used to modify the display of the current cell
@@ -120,9 +128,11 @@ function modify_selected(e){
     color_value = document.getElementById("selected_border_color").value + " inset 0 0 0 4px"    
     SELECTED_ELEMENT.style.boxShadow = color_value 
     // Update the background color
-    SELECTED_ELEMENT.setAttribute("background_color", document.getElementById("selected_bg_color").value)
-    color_value = document.getElementById("selected_bg_color").value
-    SELECTED_ELEMENT.style.background = color_value 
+    if(parseInt(SELECTED_ELEMENT.getAttribute("background_index")) == 0){
+        SELECTED_ELEMENT.setAttribute("background_color", document.getElementById("selected_bg_color").value)
+        color_value = document.getElementById("selected_bg_color").value
+        SELECTED_ELEMENT.style.background = color_value 
+    }    
     // Adjust the font color if the color value is black
     if(DARK_COLORS.includes(color_value)){
         SELECTED_ELEMENT.style.color = COLOR_LIBRARY.white
@@ -135,15 +145,19 @@ function modify_selected(e){
 // This function is used to modify the selected cell, which will clear all the border/COLOR_LIBRARY on it
 function clear(e){
     e.preventDefault()
-    // console.log("rk")
-    // console.log(e)
-    var target_element = e.target
-    // console.log(target_element)
-    select(target_element)
-    document.getElementById("selected_border_color").value = COLOR_LIBRARY.white
-    document.getElementById("selected_bg_color").value = COLOR_LIBRARY.white
-    modify_selected(target_element)
-}
+    if(CURRENT_MODE ==MODE_DICT.content){
+        // console.log("rk")
+        // console.log(e)
+        var target_element = e.target
+        // console.log(target_element)
+        select(target_element)
+        document.getElementById("selected_border_color").value = COLOR_LIBRARY.white
+        document.getElementById("selected_bg_color").value = COLOR_LIBRARY.white
+        modify_selected(target_element)
+        return
+    }   
+}    
+    
 
 // This function is to handle the mouse scroll
 function scroll_handler(e){
@@ -158,6 +172,7 @@ function scroll_handler(e){
     // Under content mode:
     if (CURRENT_MODE == MODE_DICT.content){
         if(delta < 0){
+            target_element.setAttribute("background_index",0)
             // DeltaY < 0: Scroll on top
             let flag = false
             document.getElementById("selected_bg_color").value = Object.entries(COLOR_LIBRARY)[0][0]
@@ -191,9 +206,12 @@ function scroll_handler(e){
                 }
             }
         }
+
+        modify_selected(target_element)
+        return
     }
     // Under border mode, modify the thick_border iteratively
-    else{
+    if (CURRENT_MODE == MODE_DICT.border){
         let current_thick_status = target_element.getAttribute('thick_border')
         TR = current_thick_status[0]+current_thick_status[1]
         BL = current_thick_status[2]+current_thick_status[3]
@@ -215,45 +233,125 @@ function scroll_handler(e){
         // console.log(current_thick_status)
         thick_border_modifier(target_element, current_thick_status)
         target_element.setAttribute('thick_border', current_thick_status)
+
+        modify_selected(target_element)
+        return
+    }
+    if(CURRENT_MODE == MODE_DICT.image){
+        // let current_url = target_element.style.backgroundImage
+        // console.log(current_url)
+        // let index = ICON_SRC_LIST.indexOf(current_url)
+        // console.log(index+'/'+ICON_SRC_LIST.length)
+        index = parseInt(target_element.getAttribute("background_index"))
+
+        
+        if(delta<0){
+            index = (index - 1 + ICON_SRC_LIST.length) % ICON_SRC_LIST.length
+
+            // console.log(index)
+            if(index != 0){
+                target_element.style.backgroundImage = ICON_SRC_LIST[index]
+                // target_element.style.backgroundColor = 'white'
+                target_element.style.backgroundPosition = "center center"
+                target_element.style.backgroundSize = 'cover'
+            }
+            else{
+                target_element.style.backgroundImage = "none"
+            }
+            // console.log(target_element.style.backgroundImage)
+            target_element.setAttribute("background_index",index)
+        }
+        else{
+            // DeltaY > 0: Scroll to bottom
+            let flag = false
+            document.getElementById("selected_border_color").value = Object.entries(COLOR_LIBRARY)[0][0]
+            for(const [key, value] of Object.entries(COLOR_LIBRARY)){
+    
+                if(!flag && value == SELECTED_ELEMENT.getAttribute("border_color")){
+                    flag = true
+                    continue
+                }          
+                if(flag){
+                    document.getElementById("selected_border_color").value = value
+                    flag = false
+                    break
+                }
+            }
+            modify_selected()
+        }
+        
     }
     
-    modify_selected(target_element)
+    
 }
 
 function change_mode(e){
-    if(CURRENT_MODE == MODE_DICT.content){
-        CURRENT_MODE = MODE_DICT.border
-        document.getElementById("mode_name").textContent = MODE_DICT.border
-    }
-    else{
-        CURRENT_MODE = MODE_DICT.content
-        document.getElementById("mode_name").textContent = MODE_DICT.content
-    }
+
+    let mode_list = Object.keys(MODE_DICT)
+    let index = mode_list.indexOf(CURRENT_MODE)
+    index = (index + 1) % mode_list.length    
+    CURRENT_MODE = MODE_DICT[mode_list[index]]
+    document.getElementById("mode_name").textContent = MODE_DICT[mode_list[index]]
+
+
+
+    // if(CURRENT_MODE == MODE_DICT.content){
+    //     CURRENT_MODE = MODE_DICT.border
+    //     document.getElementById("mode_name").textContent = MODE_DICT.border
+    // }
+    // else{
+    //     CURRENT_MODE = MODE_DICT.content
+    //     document.getElementById("mode_name").textContent = MODE_DICT.content
+    // }
 }
+
+var BORDER_NORMAL = '2px rgb(200, 200, 200) solid'
+var BORDER_THICK =  "2px black solid"
 
 function thick_border_modifier(target, status){
     if(status[0] === "1"){
-        target.style.borderTop = "2px black solid"
+        target.style.borderTop = BORDER_THICK
     }
     else{
-        target.style.borderTop = '2px rgb(200, 200, 200) solid'
+        target.style.borderTop = BORDER_NORMAL
     }
     if(status[1] === "1"){
-        target.style.borderRight = "2px black solid"
+        target.style.borderLeft = BORDER_THICK
     }
     else{
-        target.style.borderRight = '2px rgb(200, 200, 200) solid'
+        target.style.borderLeft = BORDER_NORMAL
     }
     if(status[2] === "1"){
-        target.style.borderBottom = "2px black solid"
+        target.style.borderBottom = BORDER_THICK
     }
     else{
-        target.style.borderBottom = '2px rgb(200, 200, 200) solid'
+        target.style.borderBottom = BORDER_NORMAL
     }
     if(status[3] === "1"){
-        target.style.borderLeft = "2px black solid"
+        target.style.borderRight = BORDER_THICK
     }
     else{
-        target.style.borderLeft = '2px rgb(200, 200, 200) solid'
+        target.style.borderRight = BORDER_NORMAL
     }
+}
+
+
+
+function upload(){
+    var input = document.getElementById("img")
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            let result_src = e.target.result
+            result_src = "url('" + result_src+ "')"
+            if (!ICON_SRC_LIST.includes(result_src)){
+                ICON_SRC_LIST.push(result_src)
+            }
+            // var element = document.getElementById('imgdiv')
+            // element.style.backgroundColor = "yellow"
+            // element.style.backgroundImage = result_src
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+    // console.log(ICON_SRC_LIST)
 }
